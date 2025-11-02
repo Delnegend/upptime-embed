b4bd2f7e830378fbf57f407e07255c398acf26c1
Delnegend <kiennguyen19323@gmail.com>
2025-11-02 14:13:58 +0700

    build: rm all go 3rd party dep

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module upptime-api
 
-go 1.24.0
-
-require github.com/lmittmann/tint v1.0.7
+go 1.25.3
--- main.go
+++ main.go
@@ -4,21 +4,8 @@ import (
 	"embed"
 	"log/slog"
 	"net/http"
-	"os"
-	"time"
 	"upptime-api/routes"
-
-	"github.com/lmittmann/tint"
 )
-
-func init() {
-	slog.SetDefault(slog.New(
-		tint.NewHandler(os.Stderr, &tint.Options{
-			Level:      slog.LevelDebug,
-			TimeFormat: time.RFC1123Z,
-		}),
-	))
-}
 
 //go:embed all:frontend/.output/public
 var frontend embed.FS
